01e5c28ad9c14f82e75777f3636dbb3529aef7cb
Madusanka-MAX <isuru.rodrigo1243@gmail.com>
2025-03-28 03:41:49 +0530

    0.7

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,11 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\User;
 use Laravel\Sanctum\HasApiTokens;
-
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, Searchable;
     
     protected $fillable = [
         'title',
@@ -52,5 +52,15 @@ class Post extends Model
     public function getLikesCountAttribute()
     {
         return $this->likes()->count();
+    }
+
+    public function toSearchableArray()
+    {
+        return [
+            'title' => $this->title,
+            'content' => strip_tags($this->content),
+            'category' => $this->category->name,
+            'tags' => $this->tags->pluck('name')->join(' ')
+        ];
     }
 }
